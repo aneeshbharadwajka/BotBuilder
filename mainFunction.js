4f1a4afb2b42996d24bf9b76bf749bc5c186d428
aneeshbharadwajka <aneesh.bharadwaj7@gmail.com>
2017-02-01 12:43:18 +0530

    Added certain edge case outputs

--- mainFunction.js
+++ mainFunction.js
@@ -3,10 +3,8 @@ var path = require('path');
 
 function MoveTheBot(inputMoves, currentDirection, currentLocationX, currentLocationY, dimensionX, dimensionY) {
   var currentMove;
-  
   for (var index = 0; index < inputMoves.length; index++) {
     currentMove = inputMoves[index];
-    
     if (currentDirection === 'N') {
       if (currentMove === 'M' && currentLocationY === 0) {
         console.log('Cannot Move as end of grid is reached');
@@ -19,7 +17,7 @@ function MoveTheBot(inputMoves, currentDirection, currentLocationX, currentLocat
       }
     }
     else if (currentDirection === 'S') {
-      if (currentMove === 'M' && currentLocationY === dimensionY) {
+      if (currentMove === 'M' && currentLocationY === dimensionY - 1) {
         console.log('Cannot Move as end of grid is reached');
       } else if (currentMove === 'M') {
         currentLocationY = currentLocationY + 1;
@@ -30,7 +28,7 @@ function MoveTheBot(inputMoves, currentDirection, currentLocationX, currentLocat
       }
     }
     else if (currentDirection === 'E') {
-      if (currentMove === 'M' && currentLocationX === dimensionX) {
+      if (currentMove === 'M' && currentLocationX === dimensionX - 1) {
         console.log('Cannot Move as end of grid is reached');
       } else if (currentMove === 'M') {
         currentLocationX = currentLocationX + 1;
@@ -57,6 +55,11 @@ function MoveTheBot(inputMoves, currentDirection, currentLocationX, currentLocat
   console.log(currentLocationX, currentLocationY, currentDirection);
 }
 
+function checkInputCoordinates(initialPositionX, initialPositionY, dimensionX, dimensionY) {
+  if (initialPositionX < 0 || initialPositionX >= dimensionX || initialPositionY < 0 || initialPositionY >= dimensionY) {
+    return false;
+  }
+}
 function checkMovesValidity(inputMoves) {
   for (var index = 0; index < inputMoves.length; index++) {
     if (inputMoves[index] !== 'L' && inputMoves[index] !== 'R' && inputMoves[index] !== 'M') {
@@ -68,13 +71,11 @@ function checkMovesValidity(inputMoves) {
 
 function mainFunction() {
   var count = 0;
-  // console.log('Hello');
   var inputSize, dimensionX, dimensionY, initialPosition, initialPositionDirection; var initialPositionY, initialPositionX, inputMoves;
   var InputFile = fs.readFileSync(process.argv[2]).toString().split('\n');
 
   for (var index = 0; index < InputFile.length; index++) {
     if (InputFile[index].length !== 0) {
-      //console.log(InputFile[index]);
       if (count === 0) {
         inputSize = InputFile[index].split(' ');
         // console.log(inputSize);
@@ -87,20 +88,21 @@ function mainFunction() {
         initialPositionY = Number(initialPosition[1]);
         initialPositionDirection = initialPosition[2];
         // console.log(initialPositionX,initialPositionY,//initialPositionDirection);
-
       }
       if (count === 2) {
         inputMoves = InputFile[index];
-        //console.log(inputMoves);
-
       }
       count++;
-
     }
-
   }
   if (checkMovesValidity(inputMoves) === false) {
     console.log('The input moves provided are invalid.Please provide valid input moves');
+    process.exit(1);
+  }
+  if (checkInputCoordinates(initialPositionX, initialPositionY, dimensionX, dimensionY) === false) {
+    console.log(`Please provide valid input coordinates between 
+    X: 0 and ${dimensionX} 
+    Y: 0 and ${dimensionY}`);
     process.exit(1);
   }
   var currentDirection, currentLocationX, currentLocationY, currentInputMove;
@@ -108,12 +110,7 @@ function mainFunction() {
   currentLocationX = initialPositionX;
   currentLocationY = initialPositionY;
   MoveTheBot(inputMoves, currentDirection, currentLocationX, currentLocationY, dimensionX, dimensionY);
-  
-
-
 }
-
-
 
 mainFunction();
 
